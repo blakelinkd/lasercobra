76b8a6e1d80bc46d954ed4fed946376086887c45
Blake Link <blakelinkd@gmail.com>
2022-01-18 10:37:03 -0600

    added icons to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,23 @@
 <ul class="col-md-12">
-    <li class="row">one</li>
-    <li class="row">two</li>
-    <li class="row">three</li>
-    <li class="row">four</li>
-    <li class="row">five</li>
+    <li class="row sidebar-action">
+        <?php echo file_get_contents(get_template_directory_uri() . "/assets/svg/bx-abacus.svg"); ?>    
+    </li>
+    <li class="row sidebar-action">
+        <?php echo file_get_contents(get_template_directory_uri() . "/assets/svg/bx-accessibility.svg"); ?>    
+    </li>
+    <li class="row sidebar-action">
+        <?php echo file_get_contents(get_template_directory_uri() . "/assets/svg/bx-add-to-queue.svg"); ?>    
+    </li>
+    <li class="row sidebar-action">
+        <?php echo file_get_contents(get_template_directory_uri() . "/assets/svg/bx-adjust.svg"); ?>    
+    </li>
+    <li class="row sidebar-action">
+        <?php echo file_get_contents(get_template_directory_uri() . "/assets/svg/bx-alarm-add.svg"); ?>    
+    </li>
+    <li class="row sidebar-action">
+        <?php echo file_get_contents(get_template_directory_uri() . "/assets/svg/bx-alarm-exclamation.svg"); ?>    
+    </li>
+    <li class="row sidebar-action">
+        <?php echo file_get_contents(get_template_directory_uri() . "/assets/svg/bx-align-justify.svg"); ?>    
+    </li>
 </ul>
